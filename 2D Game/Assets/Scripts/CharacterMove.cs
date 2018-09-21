eccdda3337389741c43797e76988c1e79390123c
Unknown <jeffrey.p462@stu.nebo.edu>
2018-09-21 10:29:37 -0600

    2D Game Update1

---
+++ 2D Game/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	//Flaco Movement Variables
+	public int moveSpeed;
+	public float jumpHeight;
+	private bool doubleJump;
+
+	//Flaco Grounded Variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+
+	//Non-Slide Player
+	private float moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Flaco Jump Code
+	 if(Input.GetKeyDown (KeyCode.Space)&&grounded){
+		Jump();
+		}
+
+		// Double Jump Code
+		if(grounded)
+			doubleJump = false;
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+			Jump();
+			doubleJump = true;
+		}
+		//Non-Slide Player
+		moveVelocity = 0f;
+
+		// Flaco Move side to side
+		if(Input.GetKey (KeyCode.D)){
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed;
+		}
+		if(Input.GetKey (KeyCode.A)){
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = -moveSpeed;
+		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+	}
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+	}
+
+}
---
+++ 2D Game/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+		public static int Score;
+	
+
+		Text ScoreText1;
+
+		// Use this for initialization
+		void Start () {
+			ScoreText1 = GetComponent<Text>();
+
+			Score = 0
+		}
+
+		void Update () {
+			if (Score < 0)
+				Score = 0;
+
+			ScoreText1.text = " " + Score;
+		}
+		
+	}
+}
