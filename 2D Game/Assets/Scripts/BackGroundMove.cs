52e4635e5046bfb78b364e515f010ff4a21c8c22
Unknown <jeffrey.p462@stu.nebo.edu>
2018-11-08 10:29:19 -0700

    2D Game Update 11

---
+++ 2D Game/Assets/Scripts/BackGroundMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour {
+
+	public CharacterMove Player;
+
+	public bool isFollowing;
+
+
+	// Camera position offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<CharacterMove>();
+
+		isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			xOffset = Player.GetComponent<Rigidbody2D>().velocity.x / 1;
+			yOffset = Player.GetComponent<Rigidbody2D>().velocity.y / 1;
+			transform.position = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z);	
+			Debug.Log(xOffset);	
+		}
+	}
+}
